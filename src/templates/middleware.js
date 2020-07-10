ab0f84bb8184e30f242f5f10d67ddc47f3eb5cd3
lazos <lazos@lazos.me>
2020-07-10 12:41:21 +0200

    remove log line

--- src/templates/middleware.js
+++ src/templates/middleware.js
@@ -2,5 +2,4 @@ import Middleware from '../../middleware'
 import options from './options'
 
 Middleware[options.namespace] = function NuxtGettextMiddleware (...args) {
-    console.log('[gettext middleware]')
 }
